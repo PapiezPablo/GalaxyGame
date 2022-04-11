2eb9566f0be443a1a2f7d510f88e46a2024242a8
PapiezPablo <maniek999zano@gmail.com>
2022-04-11 20:45:55 +0200

    Start lose life

--- engine.js
+++ engine.js
@@ -14,6 +14,21 @@ const backArt = new Image(); backArt.src = "./IMG/back.jpg";
 
 function back(){
     ctx.drawImage(backArt,0,0,cw,ch);
+    ctx.fillStyle = 'white';
+    ctx.font = "Italic 50px Times New Roman";
+    ctx.textAlign = "left";
+    if(score >= 100){
+        ctx.font = "Italic 40px Times New Roman";
+    }else if (score >= 1000){
+        ctx.font = "Italic 30px Times New Roman";
+    }
+    ctx.fillText(score,900,40);
+
+    //Life
+   
+    ctx.drawImage(lifeTab[0],30,150,heartSize1,heartSize1);
+    ctx.drawImage(lifeTab[1],30,200,heartSize2,heartSize2);
+    ctx.drawImage(lifeTab[2],30,250,heartSize3,heartSize3);
 }
 
 //Ship
@@ -69,7 +84,7 @@ const move = (e) => {
     let shotX = shipX + 80;
     let shotY = 200;
     let shotSize = 10;
-    let shotSpeed = 3;
+    let shotSpeed = 5;
     let shotBall = false;
     let blockShotY = false;
     function shot(){
@@ -103,7 +118,7 @@ const move = (e) => {
     function spawnEnemy(){
         ctx.drawImage(tableEnemy[losEnemy],enemyX,tableY[losEnemyY],enemySize,enemySize);
         enemyX -= speedEnemy;
-        if(enemyX <= 0){
+        if(enemyX <= -100){
             enemySize = 30;
             enemyX = 1024;
             losEnemyY = getRandomIntInclusive(0,14);
@@ -114,33 +129,79 @@ const move = (e) => {
         
 
     }
+    //Score
+    let score = 0;
+    function getScore(){
+        score += 10;
+    }
     
+    //Life
+    const life = new Image(); life.src = "./IMG/heart.png";
+    let heartSize1 = 30;
+    let heartSize2 = 30;
+    let heartSize3 = 30;
+    let heartSizeAll = 3;
+    let lifeTab = [life,life,life];
+    let lifeLose = true;
+    function loseLife(){
+        if(lifeLose == true){
+        if((enemyX <= shipX + shipH)&& (tableY[losEnemyY] + enemySize / 2 >= shipY) && (tableY[losEnemyY] + enemySize / 2 <= shipY + shipH) && (enemyX >= shipX)){
+            lifeLose = false;
+            heartSizeAll -= 1;
+            if(lifeLose == false){
+                let x = setInterval(() =>{},10);
+                setTimeout(() =>{lifeLose = true;clearInterval(x);},10000);
+                }
+                console.log(lifeLose);
+        }
+    }
+    }
     
-    
+
     //Engine
     function Engine() {
         back();
         ship();
         spawnEnemy();
-    
+        loseLife();
         
         if(shotBall == true){
            shot();
-            if(shotX >= 1024){
-
+            if((shotX <= enemyX + enemySize)&& (shotY + shotSize / 2 >= tableY[losEnemyY]) && (shotY + shotSize / 2 <= tableY[losEnemyY] + enemySize) && (shotX >= enemyX)){
+                //Shot
                 shotX = shipX + 80;
                 shotSize = 0;
                 blockShotY = false;
                 shotBall = false;
+
+                //Enemy
+                enemySize = 30;
+                enemyX = 1024;
+                losEnemyY = getRandomIntInclusive(0,14);
+                losEnemy = getRandomIntInclusive(0,2);
+                spawnEnemy();
+
+                //Score
+                getScore();
+                console.log(score);
+
+                
             
                 
                 
+            }else if(shotX >= 1024){
+                shotX = shipX + 80;
+                shotSize = 0;
+                blockShotY = false;
+                shotBall = false;
             }
         
         }
         if(blockShotY == false){
             shotY = shipY + (shipH / 2);
             };
+
+            
 
     
 }
